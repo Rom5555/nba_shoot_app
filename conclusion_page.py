558dd9bb4723b165af6d78b32a9a48d08daee1c8
Rom5555 <155468908+Rom5555@users.noreply.github.com>
2025-10-03 14:58:59 +0400

    Version correct avec introduction et rapport final

--- conclusion_page.py
+++ conclusion_page.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 def show():
-    with open("fichiers_markdown/Rapport Final.md", "r", encoding="utf-8") as f:
+    with open("fichiers_markdown/rapport_final.md", "r", encoding="utf-8") as f:
         st.markdown(f.read(), unsafe_allow_html=True)
